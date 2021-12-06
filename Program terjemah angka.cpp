097bd94adf118681c10da03d20220acf342cfe8a
Shela Rana Kezyah <92391060+shelakzy@users.noreply.github.com>
2021-12-06 12:03:13 +0700

    Add files via upload

---
+++ Program terjemah angka.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+	
+   // cout<< "Nama : Shela Rana Kezyah " <<endl;
+   // cout<< "NPM  : 2157051007        " <<endl;
+   // cout<< "Kelas: A S1 Ilmu Komputer" <<endl;
+   // cout<<     " POST-TEST 3  "         <<endl;
+   
+   int a;
+   //masukkan nominal//
+   cout<<"Masukkan Angka Bilangan :"<<endl;
+   cin>> a;
+   
+   if(a == 1)
+   cout<<"Satu"<<endl;
+   else if(a == 2)
+   cout<<"Dua"<<endl;
+   else if(a == 3)
+   cout<<"Tiga"<<endl;
+   else if(a == 4)
+   cout<<"Empat"<<endl;
+   else if(a == 5)
+   cout<<"Lima"<<endl;
+   else if(a == 6)
+   cout<<"Enam"<<endl;
+   else if(a == 7)
+   cout<<"Tujuh"<<endl;
+   else if(a == 8)
+   cout<<"Delapan"<<endl;
+   else if(a == 9)
+   cout<<"Sembilan"<<endl;
+   else
+   cout<<a<<" "<<endl;
+   
+   return 0;
+}
